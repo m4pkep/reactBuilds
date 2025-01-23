e1de527164c0158d2d36fc479ded603af2756837
m4pkep <145787533+m4pkep@users.noreply.github.com>
2025-01-23 17:40:58 +0300

    password generator

--- src/App.js
+++ src/App.js
@@ -3,12 +3,39 @@ import './index.scss';
 function App() {
   return (
     <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+      <span className="subtitle">Пароль:</span>
+      <h2>2UYsAhMdPR7wBHwG</h2>
+      <div className="row">
+        <span>Длина паролья:</span>
+        <input className="pwg-length" type="text" value="10" />
       </div>
+      <div className="row">
+        <span>Прописные буквы:</span>
+        <input class="styled-checkbox" id="checkbox1" type="checkbox" value="value1" />
+        <label for="checkbox1" />
+      </div>
+      <div className="row">
+        <span>Строчные буквы:</span>
+        <input
+          checked={true}
+          class="styled-checkbox"
+          id="checkbox2"
+          type="checkbox"
+          value="value1"
+        />
+        <label for="checkbox2" />
+      </div>
+      <div className="row">
+        <span>Цифры:</span>
+        <input class="styled-checkbox" id="checkbox3" type="checkbox" value="value1" />
+        <label for="checkbox3" />
+      </div>
+      <div className="row">
+        <span>Символы:</span>
+        <input class="styled-checkbox" id="checkbox4" type="checkbox" value="value1" />
+        <label for="checkbox4" />
+      </div>
+      <button>Сгенерировать</button>
     </div>
   );
 }
